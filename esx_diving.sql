d53b8863790d5f0be3000e1e73bf5cefce4e3b83
Tracid <tracid56@gmail.com>
2018-02-14 10:33:50 +0100

    Update esx_diving.sql

--- esx_diving.sql
+++ esx_diving.sql
@@ -1,5 +1,13 @@
-INSERT INTO `item` (name, item, price) VALUES
-	('plongee1','Plongee courte',250),
-	('plongee2','Plongee longue',350)
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('plongee1', 'Plongee courte', -1, 0, 1),
+	('plongee2', 'Plongee longue', -1, 0, 1)
 ;
 
+INSERT INTO `shops` (name, item, price) VALUES
+	('LTDgasoline', 'plongee1', 35000),
+	('RobsLiquor', 'plongee1', 35000),
+	('TwentyFourSeven', 'plongee1', 35000),
+	('LTDgasoline', 'plongee2', 50000),
+	('RobsLiquor', 'plongee2', 50000),
+	('TwentyFourSeven', 'plongee2', 50000)
+;
