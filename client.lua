5227755f2a87ebde3a180ef31cced06d67793387
Tracid <tracid56@gmail.com>
2018-02-14 10:57:31 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -16,36 +16,36 @@ AddEventHandler('esx_tenues:settenueplongee', function()
 
     		if skin.sex == 0 then
         		local clothesSkin = {
-            		['tshirt_1'] 	= 15, ['tshirt_2'] 	= 0,
-					['ears_1'] = -1, ['ears_2'] = 0,
-            		['torso_1'] 	= 15, ['torso_2'] 	= 0,
-            		['decals_1'] 	= 0,  ['decals_2'] 	= 0,
-            		['mask_1'] 		= 36, ['mask_2'] 	= 0,
-            		['arms'] 		= 15,
-            		['pants_1'] 	= 16, ['pants_2'] 	= 0,
-            		['shoes_1'] 	= 34, ['shoes_2'] 	= 0,
-            		['helmet_1'] 	= 8, ['helmet_2'] 	= 0,
-            		['bags_1']		= 43, ['bags_2']	= 0,
-					['glasses_1'] = 6, ['glasses_2'] = 0,
-					['chain_1'] = 0, ['chain_2'] = 0,
-            		['bproof_1'] 	= 0,  ['bproof_2'] 	= 0
+            		['tshirt_1'] = 15, ['tshirt_2'] = 0,
+			['ears_1'] = -1, ['ears_2'] = 0,
+            		['torso_1'] = 15, ['torso_2'] = 0,
+            		['decals_1'] = 0,  ['decals_2']= 0,
+            		['mask_1'] = 36, ['mask_2'] = 0,
+            		['arms'] = 15,
+            		['pants_1'] = 16, ['pants_2'] = 0,
+            		['shoes_1'] = 34, ['shoes_2'] = 0,
+            		['helmet_1'] 	= 8, ['helmet_2'] = 0,
+            		['bags_1'] = 43, ['bags_2'] = 0,
+			['glasses_1'] = 6, ['glasses_2'] = 0,
+			['chain_1'] = 0, ['chain_2'] = 0,
+            		['bproof_1'] = 0,  ['bproof_2'] = 0
         		}
         		TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
     		else
         		local clothesSkin = {
-            		['tshirt_1'] 	= 15, ['tshirt_2'] 	= 0,
-					['ears_1'] = -1, ['ears_2'] = 0,
-            		['torso_1'] 	= 15, ['torso_2'] 	= 0,
-            		['decals_1'] 	= 0,  ['decals_2'] 	= 0,
-            		['mask_1'] 		= 36, ['mask_2'] 	= 0,
-            		['arms'] 		= 15,
-            		['pants_1'] 	= 15, ['pants_2'] 	= 0,
-            		['shoes_1'] 	= 35, ['shoes_2'] 	= 0,
-            		['helmet_1'] 	= -1, ['helmet_2'] 	= 0,
-            		['bags_1']		= 43, ['bags_2']	= 0,
-					['glasses_1'] = 5, ['glasses_2'] = 0,
-					['chain_1'] = 0, ['chain_2'] = 0,
-            		['bproof_1'] 	= 0,  ['bproof_2'] 	= 0
+            		['tshirt_1'] = 15, ['tshirt_2'] = 0,
+			['ears_1'] = -1, ['ears_2'] = 0,
+            		['torso_1'] = 15, ['torso_2'] 	= 0,
+            		['decals_1'] = 0,  ['decals_2'] = 0,
+            		['mask_1'] = 36, ['mask_2'] 	= 0,
+            		['arms'] = 15,
+            		['pants_1'] = 15, ['pants_2'] 	= 0,
+            		['shoes_1'] = 35, ['shoes_2'] 	= 0,
+            		['helmet_1']= -1, ['helmet_2'] 	= 0,
+            		['bags_1'] = 43, ['bags_2']	= 0,
+			['glasses_1'] = 5, ['glasses_2'] = 0,
+			['chain_1'] = 0, ['chain_2'] = 0,
+            		['bproof_1'] = 0,  ['bproof_2'] = 0
         		}
         		TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
         	end
@@ -82,36 +82,36 @@ AddEventHandler('esx_tenues:settenueplongee', function()
 
     		if skin.sex == 0 then
         		local clothesSkin = {
-            		['tshirt_1'] 	= 15, ['tshirt_2'] 	= 0,
-					['ears_1'] = -1, ['ears_2'] = 0,
-            		['torso_1'] 	= 15, ['torso_2'] 	= 0,
-            		['decals_1'] 	= 0,  ['decals_2'] 	= 0,
-            		['mask_1'] 		= 36, ['mask_2'] 	= 0,
+            		['tshirt_1'] 	= 15, ['tshirt_2'] = 0,
+			['ears_1'] = -1, ['ears_2'] = 0,
+            		['torso_1'] 	= 15, ['torso_2'] = 0,
+            		['decals_1'] 	= 0,  ['decals_2'] = 0,
+            		['mask_1'] 		= 36, ['mask_2'] = 0,
             		['arms'] 		= 15,
-            		['pants_1'] 	= 16, ['pants_2'] 	= 0,
-            		['shoes_1'] 	= 34, ['shoes_2'] 	= 0,
-            		['helmet_1'] 	= 8, ['helmet_2'] 	= 0,
-            		['bags_1']		= 43, ['bags_2']	= 0,
-					['glasses_1'] = 6, ['glasses_2'] = 0,
-					['chain_1'] = 0, ['chain_2'] = 0,
+            		['pants_1'] 	= 16, ['pants_2'] = 0,
+            		['shoes_1'] 	= 34, ['shoes_2'] = 0,
+            		['helmet_1'] 	= 8, ['helmet_2'] = 0,
+            		['bags_1']		= 43, ['bags_2'] = 0,
+			['glasses_1'] = 6, ['glasses_2'] = 0,
+			['chain_1'] = 0, ['chain_2'] = 0,
             		['bproof_1'] 	= 0,  ['bproof_2'] 	= 0
         		}
         		TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
     		else
         		local clothesSkin = {
-            		['tshirt_1'] 	= 15, ['tshirt_2'] 	= 0,
-					['ears_1'] = -1, ['ears_2'] = 0,
-            		['torso_1'] 	= 15, ['torso_2'] 	= 0,
-            		['decals_1'] 	= 0,  ['decals_2'] 	= 0,
-            		['mask_1'] 		= 36, ['mask_2'] 	= 0,
-            		['arms'] 		= 15,
-            		['pants_1'] 	= 15, ['pants_2'] 	= 0,
-            		['shoes_1'] 	= 35, ['shoes_2'] 	= 0,
-            		['helmet_1'] 	= -1, ['helmet_2'] 	= 0,
-            		['bags_1']		= 43, ['bags_2']	= 0,
-					['glasses_1'] = 5, ['glasses_2'] = 0,
-					['chain_1'] = 0, ['chain_2'] = 0,
-            		['bproof_1'] 	= 0,  ['bproof_2'] 	= 0
+            		['tshirt_1'] 	= 15, ['tshirt_2'] = 0,
+			['ears_1'] = -1, ['ears_2'] = 0,
+            		['torso_1'] = 15, ['torso_2'] = 0,
+            		['decals_1'] = 0,  ['decals_2'] = 0,
+            		['mask_1'] = 36, ['mask_2'] = 0,
+            		['arms'] = 15,
+            		['pants_1'] = 15, ['pants_2'] = 0,
+            		['shoes_1'] = 35, ['shoes_2'] = 0,
+            		['helmet_1'] = -1, ['helmet_2']	= 0,
+            		['bags_1'] = 43, ['bags_2'] = 0,
+			['glasses_1'] = 5, ['glasses_2'] = 0,
+			['chain_1'] = 0, ['chain_2'] = 0,
+            		['bproof_1'] = 0,  ['bproof_2'] = 0
         		}
         		TriggerEvent('skinchanger:loadClothes', skin, clothesSkin)
         	end
