543f204809ac7e1510e9405cf6097f9f71017510
rex2630 <rex2630@gmail.com>
2020-02-07 16:22:00 +0100

    Weight support

--- esx_diving.sql
+++ esx_diving.sql
@@ -1,6 +1,6 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-	('plongee1', 'Plongee courte', -1, 0, 1),
-	('plongee2', 'Plongee longue', -1, 0, 1)
+INSERT INTO `items` (name, label, weight, rare, can_remove) VALUES
+	('plongee1', 'Plongee courte', 1, 0, 1),
+	('plongee2', 'Plongee longue', 1, 0, 1)
 ;
 
 INSERT INTO `shops` (name, item, price) VALUES
